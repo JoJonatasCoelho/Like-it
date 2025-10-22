57fd1afcd101f6d520e007a632c638c4ccb9cf1f
VictorFranklinM <victorfranklin2006@gmail.com>
2025-10-22 17:30:46 -0300

    feat: background music

--- Scripts/radio.gd
+++ Scripts/radio.gd
@@ -2,15 +2,13 @@ extends Interactable
 
 @onready var audio_player : AudioStreamPlayer3D = $AudioStreamPlayer3D
 
-@export var is_playing = true
-
 func _ready():
 	audio_player.stream = preload("res://Assets/audio/POL-elevator-lift2.wav")
 	audio_player.play(0)
 
 func interact(_caller):
-	is_playing = !is_playing
-	if is_playing:
+	audio_player.playing = !audio_player.playing
+	if audio_player.playing:
 		audio_player.play(0)
-	if !is_playing:
+	if !audio_player.playing:
 		audio_player.stop()
