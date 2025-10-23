12488c13ca71fe060fd3883941e2b39226216a8e
John <jonatascleytonf@gmail.com>
2025-10-23 00:21:39 -0300

    feat: menu, intro, and main level

---
+++ Scripts/menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/StartButton.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
+
+func _on_credits_button_pressed() -> void:
+	pass # Ainda precisa implementar 
