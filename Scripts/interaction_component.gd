ed33a08bfad61bec737a900146b2223d0de1966f
John <jonatascleytonf@gmail.com>
2025-10-23 07:46:57 -0300

    feat: better interactibles

--- Scripts/interaction_component.gd
+++ Scripts/interaction_component.gd
@@ -3,7 +3,6 @@ extends Node3D
 @onready var interaction_raycast : RayCast3D = $"../RayCast3D"
 @onready var interaction_label : Label = $"../../Interaction Label"
 var interaction_is_reset : bool = true
-var is_interacting : bool = false
 
 func _process(_delta):
 	if interaction_raycast.is_colliding():
@@ -20,14 +19,14 @@ func _process(_delta):
 			interaction_is_reset = true
 
 func _input(event):
-	if is_interacting:
-		return
 		
 	if event.is_action_pressed("interact"):
 		if interaction_raycast.is_colliding():
 			var interactable = interaction_raycast.get_collider()
-			if interactable.has_method("interact"):
-				is_interacting = true
+			if interactable is Interactable:
+				if interactable.is_interacting:
+					return
+				interactable.is_interacting = true
 				interaction_label.text = ""  
 				await interactable.interact(self)
-				is_interacting = false
+				interactable.is_interacting = false
--- level.gd
+++ level.gd
@@ -7,8 +7,8 @@ extends Node3D
 func _ready() -> void:
 	player.can_move = false
 	player.can_look = false
-	player_animator.play("standingUp")
-	await get_tree().create_timer(11.4).timeout
+	#player_animator.play("standingUp")
+	#await get_tree().create_timer(11.4).timeout
 	player.can_move = true
 	player.can_look = true
 
