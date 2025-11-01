421d981424124873edbe5f55a6924b2c72f9f00e
John <jonatascleytonf@gmail.com>
2025-10-31 23:23:55 -0300

    refac: excluindo scripts redundantes

--- Scripts/Interactables/visualizable.gd
+++ Scripts/Interactables/visualizable.gd
@@ -2,7 +2,7 @@ extends Interactable
 
 @export var photo_texture: Texture2D
 @export var animation_name: String 
-@onready var ui: CanvasLayer = get_tree().root.get_node("Map/UI/CanvasLayer")
+@onready var ui: CanvasLayer = $"../../../UI/CanvasLayer"
 
 func interact(_caller):
 	var subtitle_animator: AnimationPlayer = ui.get_node("SubtitlesAnimation")
